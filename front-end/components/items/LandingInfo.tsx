0dbacae6f5e1610fdfa7e39613de3cf98df3c826
Kex027 <adafisz27@gmail.com>
2023-09-09 16:31:58 +0200

    Created responsive Landing and added font

--- front-end/components/items/LandingInfo.tsx
+++ front-end/components/items/LandingInfo.tsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 
 const LandingInfo = () => {
   return (
-    <div className="flex flex-col justify-around absolute bottom-10 left-10 gap-y-10">
-      <div className="text-8xl flex flex-col justify-center">
+    <div className="flex flex-col justify-center items-center h-full gap-4 md:items-start md:justify-end md:max-w-[400px] lg:max-w-[450px] xl:max-w-[500px]">
+      <div className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl flex flex-col justify-center">
         <span className="uppercase font-bold tracking-wider">
           GAUNTLER <br /> OUT NOW!
         </span>
       </div>
-      <h3 className="text-2xl font-bold">
-        Get Gauntler on Nintendo Switch and Steam!
+      <h3 className="text-xl lg:text-2xl font-bold">
+        On Nintendo Switch and Steam!
       </h3>
-      <label className="flex justify-evenly gap-x-10 text-center text-lg">
-        <span className="px-5 py-2 tracking-wide w-1/2 text-xl underline font-bold tracking-wider">
+      <label className="flex justify-evenly gap-x-10 text-lg self-center">
+        <span className="tracking-wide text-xl underline font-bold tracking-wider">
           <Link href="/">CHECK OUT</Link>
         </span>
-        <span className="px-5 py-2 tracking-wide w-1/2 text-xl underline font-bold tracking-wider">
+        <span className="tracking-wide text-xl underline font-bold tracking-wider">
           <Link href="/">TRAILER</Link>
         </span>
       </label>
--- front-end/pages/Games.tsx
+++ front-end/pages/Games.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NewsLetter from "../components/sections/NewsLetter";
 import GamesList from "../components/sections/GamesList";
 import AboutUs from "../components/sections/AboutUs";
+import Landing from "../components/sections/Landing";
 
 const Games = () => {
   return (
@@ -10,6 +11,7 @@ const Games = () => {
 
       {/*<NewsLetter />*/}
 
+      <Landing />
       <AboutUs />
     </>
   );
