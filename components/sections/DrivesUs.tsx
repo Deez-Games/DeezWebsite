6fa4a86a08883eeb054f10c38e5efa319a321fa9
Sajluk <100955136+saj-luk@users.noreply.github.com>
2023-11-27 20:54:58 +0100

    quick bg fixes 2

--- components/sections/DrivesUs.tsx
+++ components/sections/DrivesUs.tsx
@@ -6,19 +6,14 @@ const DrivesUs = () => {
       id="DrivesUs"
       className="flex flex-col md:flex-row text-brightBg px-4 sm:px-12 2xl:px-32 py-8 sm:py-24 gap-6 customGradientVioletRight shadow-insetBottom"
     >
-      <div className="flex justify-start items-center">
-        <div className="">
-          <h2 className="text-5xl lg:text-6xl font-bold tracking-wider">
-            WHAT DRIVES{"\u00A0"}US?
-          </h2>
-          <br />
-          <p className="text-lg lg:text-2xl xl:max-w-[60%]">
-          The main reason we decided to start our journey is the desire to create unique games that are both exciting for players and enjoyable for us to create.<br /><br />We deeply believe that the sense of creating is to break the schemes and stay unconventional.
-          </p>
-        </div>
-      </div>
-      <div className="md:max-w-[50%] flex items-center">
-        <img src="retro-gaming.png" alt="passion to games is what drives us" />
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-5xl lg:text-6xl text-center font-bold tracking-wider">
+          WHAT DRIVES{"\u00A0"}US?
+        </h2>
+        <br />
+        <p className="text-center text-lg md:w-1/2 lg:text-2xl">
+        The main reason we decided to start our journey is the desire to create unique games that are both exciting for players and enjoyable for us to create.<br /><br />We deeply believe that the sense of creating is to break the schemes and stay unconventional.
+        </p>
       </div>
     </div>
   );
