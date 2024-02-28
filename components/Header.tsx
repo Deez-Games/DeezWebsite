7f76463db34e65b090a9c404b8ad31062157996e
Kex027 <adafisz27@gmail.com>
2024-02-28 18:50:17 +0100

    Fixed navbar background in mobile, removed logo padding, optimized code,

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
@@ -12,64 +11,55 @@ const Header = () => {
       setShowHeaderBgPx(window.innerHeight - 10);
 
       window.addEventListener("scroll", () =>
-        setScrollOnTop(window.pageYOffset <= showHeaderBgPx)
+        setScrollOnTop(window.scrollY <= showHeaderBgPx)
       );
     }
   }, [showHeaderBgPx]);
 
   function showSideNav() {
     const sideNav = document.querySelector(".side-nav");
-    sideNav.classList.replace('translate-x-[100vw]','translate-x-0');
+    sideNav.classList.replace("translate-x-[100vw]", "translate-x-0");
   }
 
   return (
     <div
       className={`flex justify-center items-center w-full opacity-1 py-5 fixed top-0 z-10 ${
-        scrollOnTop ? "" : "bg-darkGrey bg-opacity-60"
+        scrollOnTop ? "" : "md:bg-darkGrey md:bg-opacity-60"
       }`}
     >
       <div className="w-[95%] flex justify-between">
-        <div className="pl-10">
-          <Link href="/">
-            <img
-              className="cursor-pointer"
-              src="logo.png"
-              width={30}
-              height={30}
-              alt="Deez Games logo"
-            />
-          </Link>
-        </div>
+        <Link href="/">
+          <img
+            className="cursor-pointer"
+            src="/logo.png"
+            width={30}
+            height={30}
+            alt="Deez Games logo"
+          />
+        </Link>
         <div className="hidden md:flex w-1/3 lg:w-1/4 justify-between gap-2">
-          <Link href="/">
-            <span className="hover:text-textHover font-bold cursor-pointer">
-              HOME
-            </span>
-          </Link>
-          <Link href="/#Games" scroll={false}>
-            <span className="hover:text-textHover font-bold cursor-pointer">
-              GAMES
-            </span>
-          </Link>
-          <Link href="/#AboutUs" scroll={false}>
-            <span className="hover:text-textHover font-bold cursor-pointer">
-              ABOUT
-            </span>
-          </Link>
-          <Link href="/#Contact" scroll={false}>
-            <span className="hover:text-textHover font-bold cursor-pointer">
-              CONTACT
-            </span>
-          </Link>
+          <HeaderLink text="HOME" href={"/"} />
+          <HeaderLink text="GAMES" href={"/#Games"} />
+          <HeaderLink text="ABOUT" href={"/#AboutUs"} />
+          <HeaderLink text="CONTACT" href={"/#Contact"} />
         </div>
         <div className="flex md:hidden justify-between text-3xl">
-          <div></div>
           <button onClick={showSideNav}>
             <RxHamburgerMenu className="text-white" />
           </button>
         </div>
       </div>
     </div>
+  );
+};
+
+const HeaderLink = ({ text, href }) => {
+  return (
+    <Link href={href} scroll={text === "HOME"}>
+      <span className="hover:text-textHover font-bold cursor-pointer">
+        {text}
+      </span>
+    </Link>
   );
 };
 
--- components/sections/SideNav.tsx
+++ components/sections/SideNav.tsx
@@ -2,34 +2,35 @@ import Link from "next/link";
 import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
+function hideSideNav() {
+  const sideNav = document.querySelector(".side-nav");
+  sideNav.classList.replace("translate-x-0", "translate-x-[100vw]");
+}
+
 const OurValues = () => {
-
-  function hideSideNav() {
-    const sideNav = document.querySelector(".side-nav");
-    sideNav.classList.replace('translate-x-0','translate-x-[100vw]');
-  }
-
   return (
     <div className="side-nav fixed flex flex-col text-right text-3xl md:hidden top-0 left-0 translate-x-[100vw] transition w-full h-screen bg-darkBg p-4 z-10">
-      <button className="flex justify-end pb-10" onClick={hideSideNav}>
+      <button className="flex justify-end pb-4" onClick={hideSideNav}>
         <AiOutlineClose />
       </button>
-      <Link className="link" href="/#AboutUs">
-        <span onClick={hideSideNav} className="hover:text-textHover cursor-pointer border-b-[1px] p-2 py-2">
-          ABOUT
-        </span>
-      </Link>
-      <Link className="link" href="/#Games">
-        <span onClick={hideSideNav} className="hover:text-textHover cursor-pointer border-b-[1px] p-2 py-2">
-          GAMES
-        </span>
-      </Link>
-      <Link className="link" href="/#Contact">
-        <span onClick={hideSideNav} className="hover:text-textHover cursor-pointer border-b-[1px] p-2 py-2">
-          CONTACT
-        </span>
-      </Link>
+      <HeaderLink text={"HOME"} href={"/"} />
+      <HeaderLink text={"GAMES"} href={"/#Games"} />
+      <HeaderLink text={"ABOUT"} href={"/#AboutUs"} />
+      <HeaderLink text={"CONTACT"} href={"/#Contact"} />
     </div>
+  );
+};
+
+const HeaderLink = ({ text, href }) => {
+  return (
+    <Link className="link" href={href} scroll={text === "HOME"}>
+      <span
+        onClick={hideSideNav}
+        className="hover:text-textHover cursor-pointer p-2 py-2"
+      >
+        {text}
+      </span>
+    </Link>
   );
 };
 
